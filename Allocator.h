6518295391d310fca893c4eb7109b91e9fce7f9e
Alex Forsythe <alexwforsythe@gmail.com>
2014-10-20 14:39:10 -0500

    Implemented allocate function (not tested)

--- Allocator.h
+++ Allocator.h
@@ -113,9 +113,42 @@ class Allocator {
          * return 0, if allocation fails
          */
         pointer allocate (size_type n) {
-            // <your code>
-            assert(valid());
-            return 0;}                   // replace!
+            assert(n > 0);
+            int sent = sizeof(int);
+            int valid_size = sizeof(T) + (2 * sent);
+            int min_size = (n * sizeof(T)) + (2 * sent);
+
+            int i = 0;
+            int val;
+            while (i < N) {
+                val = view(i);
+
+                if (val >= min_size) {
+                    int new_val, end;
+                    if (val < min_size + valid_size) {
+                        // allocate the whole block
+                        new_val = -val;
+                        end = i + val + sent;
+                    }
+                    else {
+                        // only allocate as many bytes as requested
+                        new_val = n * sizeof(T);
+                        end = i + new_val + sent;
+                    }
+
+                    a[i] = new_val;
+                    a[end] = new_val;
+                    assert(valid());
+                    return reinterpret_cast<T*>(&a[i + sent]);
+                }
+
+                // no space, increment i to next block
+                i += val + (2 * sent);
+            }
+
+            // not enough space
+            // TODO: or throw bad_alloc?
+            return 0;}
 
         // ---------
         // construct
